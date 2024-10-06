be387e7141255949ab2f16d8e8ce3ef4c9f9bc26
Henrique R Lelis <Henrique R Lelis@DESKTOP-OF8TKTP>
2024-10-06 15:13:30 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace POC.WEB_BACK_END_HENRIQUE_LELIS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
